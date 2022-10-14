d7af6ef35b713b9d23e58cb0e633cd1895321027
abdulazizbirhanu <abdulazizbirhanu@gmail.com>
2022-10-14 05:19:44 -0700

    numbers with printf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the progra's descriptions'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+while (n <= 9)
+{
+	printf("%d", n);
+	n++;
+}
+	putchar('\n');
+	return (0);
+}
