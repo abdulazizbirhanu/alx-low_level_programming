ebed332ea8ab5765760d43da9ccdddaa92e6d0d3
abdulazizbirhanu <abdulazizbirhanu@gmail.com>
2022-10-14 05:53:27 -0700

    numbers separate by , and space char

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the progra's descriptions'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+while (n <= 9)
+{
+	putchar((n % 10) + '0');
+	putchar(',');
+	putchar(' ');
+	n++;
+}
+	putchar('\n');
+	return (0);
+}
