049f4d5aaa98558e4b0c129254205ce17e64323a
abdulazizbirhanu <abdulazizbirhanu@gmail.com>
2022-10-14 04:46:49 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the progra's descriptions'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+while (c <= 'z')
+{
+	putchar(c);
+	c++;
+}
+	putchar('\n');
+	return (0);
+}
