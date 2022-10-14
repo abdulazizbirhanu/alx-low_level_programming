cb53292c26e27c9953aa298e8ae13f821d2bd41d
abdulazizbirhanu <abdulazizbirhanu@gmail.com>
2022-10-14 05:10:09 -0700

    skip e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the progra's descriptions'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+while (c <= 'z')
+{
+	if (c != 'e' && c != 'q')
+	{
+		putchar(c);
+	}
+	c++;
+}
+	putchar('\n');
+	return (0);
+}
