c607b4e4add44b8e15d0f959f2916627403b9e2f
abdulazizbirhanu <abdulazizbirhanu@gmail.com>
2022-10-18 11:44:59 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: the charactr to be checked
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(char c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90) ||
+	(c >= 47 && c <= 57))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
