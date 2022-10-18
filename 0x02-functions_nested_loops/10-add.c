6a25dbed30e6df46ca413f4a9297591f6f88cfc1
abdulazizbirhanu <abdulazizbirhanu@gmail.com>
2022-10-18 12:41:56 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+
+/**
+ * add - add two numbers
+ * @num1: first number
+ * @num2: second number
+ *
+ * Return: Always return intger
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
