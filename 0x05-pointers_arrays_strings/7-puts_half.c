8d9b96e5c1205109d7c56ec3db9af7af3b65bb0c
ABDULAZIZ BIRHANU DAWUD <abdulazizbirhanu@gmail.com>
2022-10-24 21:42:57 +0300

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of a string
+ * @str: string will be split
+ *
+ * Return: ALways nothing
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+
+	}
+	_putchar('\n');
+}
