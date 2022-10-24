4e934caa60cc2e5e080a4645b97477da888b75f3
ABDULAZIZ BIRHANU DAWUD <abdulazizbirhanu@gmail.com>
2022-10-24 21:41:03 +0300

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string will be reverse
+ *
+ * Return: Always nothing
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (len--; len >= 0; len--)
+		_putchar(s[len]);
+
+	_putchar('\n');
+}
